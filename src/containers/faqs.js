6f6444925b7714cdf16afc7554b8fee8053fbfed
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2020-09-23 12:49:19 +0300

    Added Optform in site and styled. 🐱‍🐉

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqData from '../fixtures/faqs.json';
 
 export function FaqContainer() {
@@ -14,6 +14,15 @@ export function FaqContainer() {
           </Accordion.Item>
         ))}
       </Accordion.Frame>
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
